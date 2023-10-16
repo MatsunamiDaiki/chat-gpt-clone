4a96da59b166b25c54e3824b37960693a664415f
MatsunamiDaiki <lax.md0@gmail.com>
2023-10-16 16:22:04 +0900

    openAPI連携

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import { useAppContext } from "@/context/AppContext";
-import { Timestamp, addDoc, collection, doc, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
 import React, { useEffect, useRef, useState } from "react";
 import { FaBeer, FaPaperPlane, FaTruckLoading } from "react-icons/fa";
 import { db } from "../../../firebase";
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { useAppContext } from "@/context/AppContext";
+import OpenAI from "openai";
+// import LoadingIcons from "react-loading-icons";
 
 type Message = {
   text: string;
@@ -13,11 +24,19 @@ type Message = {
 };
 
 const Chat = () => {
+  const openai = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
   const { selectedRoom, selectRoomName } = useAppContext();
   const [inputMessage, setInputMessage] = useState<string>("");
   const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const scrollDiv = useRef<HTMLDivElement>(null);
+
+  //各Roomにおけるメッセージを取得
   useEffect(() => {
     if (selectedRoom) {
       const fetchMessages = async () => {
@@ -40,9 +59,18 @@ const Chat = () => {
     }
   }, [selectedRoom]);
 
+  useEffect(() => {
+    if (scrollDiv.current) {
+      const element = scrollDiv.current;
+      element.scrollTo({
+        top: element.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
 
   const sendMessage = async () => {
-    if(!inputMessage.trim()) return;
+    if (!inputMessage.trim()) return;
 
     const messageData = {
       text: inputMessage,
@@ -50,18 +78,36 @@ const Chat = () => {
       createdAt: serverTimestamp(),
     };
 
+    //メッセージをFirestoreに保存
     const roomDocRef = doc(db, "rooms", selectedRoom!);
-    const messageCollectionRef = collection(roomDocRef, "message");
+    const messageCollectionRef = collection(roomDocRef, "messages");
     await addDoc(messageCollectionRef, messageData);
-  }
+
+    setInputMessage("");
+    setIsLoading(true);
+
+    //OpenAIからの返信
+    const gpt3Response = await openai.chat.completions.create({
+      messages: [{ role: "user", content: inputMessage }],
+      model: "gpt-3.5-turbo",
+    });
+
+    setIsLoading(false);
+
+    const botResponse = gpt3Response.choices[0].message.content;
+    await addDoc(messageCollectionRef, {
+      text: botResponse,
+      sender: "bot",
+      createdAt: serverTimestamp(),
+    });
+  };
 
   return (
     <div className="bg-gray-500 h-full p-4 flex flex-col">
       <h1 className="text-2xl text-white font-semibold mb-4">
-        タイトル
+        {selectRoomName}
       </h1>
-
-      <div className="flex-grow overflow-y-auto mb-4">
+      <div className="flex-grow overflow-y-auto mb-4" ref={scrollDiv}>
         {messages.map((message, index) => (
           <div
             key={index}
@@ -78,8 +124,8 @@ const Chat = () => {
             </div>
           </div>
         ))}
+        {/* {isLoading && <LoadingIcons.TailSpin />} */}
       </div>
-
 
       <div className="flex-shrink-0 relative">
         <input
@@ -87,6 +133,12 @@ const Chat = () => {
           placeholder="Send a Message"
           className="border-2 rounded w-full pr-10 focus:outline-none p-2"
           onChange={(e) => setInputMessage(e.target.value)}
+          value={inputMessage}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              sendMessage();
+            }
+          }}
         />
         <button
           className="absolute inset-y-0 right-4 flex items-center"
